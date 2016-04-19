6a6a20470fef975b39b2e01c8a274393c9bf85da
Riccardo Cannistrà <rickystream94@gmail.com>
2016-04-19 17:27:35 +0200

    Try message

---
+++ LM_Bressan_CoF/src/main/java/LM_Bressan_CoF/LM_Bressan_CoF/App.java
@@ -0,0 +1,13 @@
+package LM_Bressan_CoF.LM_Bressan_CoF;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

73dcd45f90c0685914ab86da04c1e3fc5a940508
pietro di marco <pietro@MacBook-Pro-di-pietro.local>
2016-04-21 13:55:01 +0200

    ci sono

--- LM_Bressan_CoF/src/main/java/LM_Bressan_CoF/LM_Bressan_CoF/App.java
+++ LM_Bressan_CoF/src/main/java/LM_Bressan_CoF/LM_Bressan_CoF/App.java
@@ -7,5 +7,6 @@ package LM_Bressan_CoF.LM_Bressan_CoF;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.print("sono dentro");
 	}
 }

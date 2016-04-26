bea56193321cb2898835a2c3e2598519abe721e0
pietro di marco <pietro@MacBook-Pro-di-pietro.local>
2016-04-26 13:42:25 +0200

    aggiunto un commento

--- LM_Bressan_CoF/src/main/java/LM_Bressan_CoF/LM_Bressan_CoF/Applicazione.java
+++ LM_Bressan_CoF/src/main/java/LM_Bressan_CoF/LM_Bressan_CoF/Applicazione.java
@@ -1,9 +1,11 @@
 package LM_Bressan_CoF.LM_Bressan_CoF;
 
-public class App {
+public class Applicazione {
+	public Applicazione() {
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.print("sono dentro");
-
 	}
 }

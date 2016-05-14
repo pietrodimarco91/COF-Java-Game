02c7df0c749632818abcc777746c66d2c204a872
Riccardo Cannistrà <rickystream94@gmail.com>
2016-05-14 02:46:55 +0200

    Completed implementation of NobilityTrack and NobilityCell.

--- LM_Bressan_CoF/src/main/java/model/NobilityTrack.java
+++ LM_Bressan_CoF/src/main/java/model/NobilityTrack.java
@@ -7,27 +7,44 @@ import java.util.*;
  * Cells.
  */
 public class NobilityTrack {
-	private static final int LENGTH=20;
+	private static final int LENGTH = 20;
 	/**
 	 * The track is an array of NobilityCell.
 	 */
 	private NobilityCell[] track;
 
 	/**
-	 * Default constructor
+	 * Default constructor: this will generate a random track, with bonus with
+	 * random positions
 	 */
-	public NobilityTrack() {
+	public NobilityTrack(int bonusNumber) {
 		track = new NobilityCell[LENGTH];
-		ArrayList<String> values = new ArrayList<String>();
-		for(NobilityCell nobilityCell:track) {
-			/*Waiting for factory method to be implemented 
-			nobilityCell = new NobilityCell(); */	
-		}
+		Random random = new Random();
+		ArrayList<String> typesOfBonus = bonusToArrayOfString();
+		int low = 0, high = typesOfBonus.size() - 1;
+		ArrayList<String> bonus = new ArrayList<String>();
 
+		for (int i=0;i<track.length;i++) {
+			if (random.nextFloat() > 0.5) {
+				for (int j = 0; j < bonusNumber; j++) {
+					bonus.add(typesOfBonus.get(random.nextInt(high - low) + low));
+				}
+				track[i] = new NobilityCell(bonus);
+				bonus.clear();
+			}
+		}
 	}
 
-	/**
-	 * 
+	/*
+	 * This method automatically generates an ArrayList<String> that contains
+	 * all the values of the enumeration.
 	 */
+	public ArrayList<String> bonusToArrayOfString() {
+		ArrayList<String> stringValues = new ArrayList<String>();
+		for (int i = 0; i < NobilityTrackBonusType.values().length; i++) {
+			stringValues.add(NobilityTrackBonusType.values()[i].toString());
+		}
+		return stringValues;
+	}
 
 }

3692e8cf634727de64a849e1f9daa12652f2c4ce
Riccardo Cannistrà <rickystream94@gmail.com>
2016-05-14 16:35:11 +0200

    Fixed NobilityTrack and NobilityTrackBonusType to avoid duplicates.

--- LM_Bressan_CoF/src/main/java/model/NobilityTrackBonusType.java
+++ LM_Bressan_CoF/src/main/java/model/NobilityTrackBonusType.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Random;
 
 /**
@@ -11,16 +12,38 @@ public enum NobilityTrackBonusType {
 	ASSISTANT, VICTORYTRACK, POLITIC, COINS, NOBILITYTRACK, DRAWPERMITTILE, BONUSPERMITTILE, TWOEMPORIUMCITY;
 
 	/**
-	 *This method is used to fill the Tiles with bonuses, in particular this method return an array from the enum in a random way.
-	 *The parameter bonusNumber indicate the size of this array.
+	 *This method is used to fill the Tiles with bonuses, in particular this method returns an array from the Enum in a random way.
+	 *@param The parameter bonusNumber indicate the size of this array.
+	 *@return the arrayList of bonuses (String)
 	 */
-	public static ArrayList random(int bonusNumber){
-		ArrayList<String> bonuses=new ArrayList();
+	public static ArrayList<String> random(int bonusNumber){
+		ArrayList<String> bonuses=new ArrayList<String>();
+		String valueToInsert;
 		for(int i=0;i<bonusNumber;i++){
-			bonuses.add(String.valueOf(values()[new Random().nextInt(values().length)]));
+			valueToInsert=String.valueOf(values()[new Random().nextInt(values().length)]);
+			while (i != 0 && bonusAlreadyExisting(bonuses, valueToInsert)) {
+				valueToInsert=String.valueOf(values()[new Random().nextInt(values().length)]);
+			}
+			bonuses.add(valueToInsert);
 		}
-
 		return bonuses;
+	}
+	
+	/**
+	 * This method is useful to check whether a specified bonus is already
+	 * inside an array or not, in order to avoid duplicates.
+	 * 
+	 * @param value
+	 *            The string to search for inside the bonus array
+	 * @return true if the value string already exists, false otherwise.
+	 */
+	public static boolean bonusAlreadyExisting(ArrayList<String> array, String value) {
+		Iterator<String> iterator = array.iterator();
+		while (iterator.hasNext()) {
+			if (iterator.next().equals(value))
+				return true;
+		}
+		return false;
 	}
 
 }

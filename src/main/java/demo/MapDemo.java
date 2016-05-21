3f25b5eeb0fbe3660d68c887cfbcbb35c12ad5a2
Riccardo Cannistrà <rickystream94@gmail.com>
2016-05-21 14:59:25 +0200

    MapDemo, attempt with different configuration parameters.

--- src/main/java/demo/MapDemo.java
+++ src/main/java/demo/MapDemo.java
@@ -5,7 +5,7 @@ import model.Map;
 public class MapDemo {
 
 	public static void main(String[] args) {
-		int numberOfPlayers=4,bonusNumber=2;
+		int numberOfPlayers=8,bonusNumber=3;
 		Map map = new Map(numberOfPlayers,bonusNumber);
 		System.out.println(map.toString());
 

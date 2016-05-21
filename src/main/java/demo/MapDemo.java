b65a57037676b31af1a4aebd999fcd3106910976
Gabriele Bressan <gabri.bressan@gmail.com>
2016-05-21 13:23:04 +0200

    Added toString() method in Region, and I modified toString method in Map added the print of regions.

--- src/main/java/demo/MapDemo.java
+++ src/main/java/demo/MapDemo.java
@@ -1,14 +1,23 @@
 package demo;
 
+import java.util.ArrayList;
+
+import model.City;
+import model.Council;
 import model.Map;
+import model.PermitTileDeck;
+import model.Region;
+import model.RegionCouncil;
 
 public class MapDemo {
 
 	public static void main(String[] args) {
-		int numberOfPlayers=4,bonusNumber=2;
-		Map map = new Map(numberOfPlayers,bonusNumber);
-		System.out.println(map.toString());
 
+		int numberOfPlayers = 4, bonusNumber = 2;
+		Map map = new Map(numberOfPlayers, bonusNumber);
+		System.out.println(map.toString());
+		Council council = new RegionCouncil();
+		PermitTileDeck permitTileDeck = new PermitTileDeck(20);
 	}
 
 }

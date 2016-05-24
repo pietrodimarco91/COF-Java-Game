554491d0e808b3add68f99fbc429297bf408fc77
Gabriele Bressan <gabri.bressan@gmail.com>
2016-05-24 14:08:00 +0200

    fixed TestClass

--- src/test/java/test/TestCheckCouncillorsSatisfaction.java
+++ src/test/java/test/TestCheckCouncillorsSatisfaction.java
@@ -3,10 +3,12 @@ package test;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import org.junit.Test;
 
 import model.Council;
+import model.Councillor;
 import model.CouncillorsPool;
 import model.PoliticCard;
 import model.Region;
@@ -25,6 +27,60 @@ public class TestCheckCouncillorsSatisfaction {
 			tempCardsArrayList.add(testPoliticCards);
 			
 		}
+		
+		int orange=0;
+		int white=0;
+		int pink=0;
+		int purple=0;
+		int black=0;
+		int multicolor=0;
+		
+		
+		for(int i=0;i<6;i++){
+		testPoliticCards= tempCardsArrayList.get(i);
+		String color=testPoliticCards.getColorCard();
+		if(color=="BLACK")
+			black++;
+		if(color=="ORANGE")
+			orange++;
+		if(color=="WHITE")
+			white++;
+		if(color=="PINK")
+			pink++;
+		if(color=="PURPLE")
+			pink++;
+		if(color=="MULTICOLOR")
+			multicolor++;
+		
+		int councillorOrange=0;
+		int councillorWhite=0;
+		int councillorPink=0;
+		int councillorPurple=0;
+		int councillorBlack=0;
+		int councillorMulticolor=0;
+		int cprrispondence=0;
+		
+		Iterator<Councillor> iterationCouncillors = testCouncil.getCouncillors().iterator();
+		Councillor councillor;
+		while(iterationCouncillors.hasNext()){
+			councillor = iterationCouncillors.next();
+			if(color=="BLACK")
+				black++;
+			if(color=="ORANGE")
+				orange++;
+			if(color=="WHITE")
+				white++;
+			if(color=="PINK")
+				pink++;
+			if(color=="PURPLE")
+				pink++;
+			if(color=="MULTICOLOR")
+				multicolor++;
+			
+		}
+			
+		}
+		
 			
 		
 	}

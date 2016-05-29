d14bace6ce1e4bbb90c01f9dd2b04399839ad6ad
Riccardo Cannistrà <rickystream94@gmail.com>
2016-05-29 18:04:28 +0200

    Completed implementation of Market and its elements

--- src/main/java/model/ConcreteItemFactory.java
+++ src/main/java/model/ConcreteItemFactory.java
@@ -1,20 +1,22 @@
 package model;
 
+import controller.Player;
+
 public class ConcreteItemFactory extends ItemFactory {
 
 	@Override
-	public ItemOnSale createPermitTileOnSale(Tile permitTile) {
-		return new PermitTileOnSale(permitTile);
+	public ItemOnSale createPermitTileOnSale(Tile permitTile,Player player,int price) {
+		return new PermitTileOnSale(permitTile,player,price);
 	}
 
 	@Override
-	public ItemOnSale createAssistantOnSale() {
-		return new AssistantOnSale();
+	public ItemOnSale createAssistantOnSale(Player player,int price) {
+		return new AssistantOnSale(player,price);
 	}
 
 	@Override
-	public ItemOnSale createPoliticCardOnSale(PoliticCard politicCard) {
-		return new PoliticCardOnSale(politicCard);
+	public ItemOnSale createPoliticCardOnSale(PoliticCard politicCard,Player player,int price) {
+		return new PoliticCardOnSale(politicCard,player,price);
 	}
 
 }

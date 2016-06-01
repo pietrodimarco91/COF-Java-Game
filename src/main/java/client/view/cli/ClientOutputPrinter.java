83d0b66996701679f340fc53d08313b07b94b935
Riccardo Cannistrà <rickystream94@gmail.com>
2016-06-01 11:49:34 +0200

    Initialized the classes for the CLI view.

---
+++ src/main/java/client/view/cli/ClientOutputPrinter.java
@@ -0,0 +1,35 @@
+package client.view.cli;
+
+import java.io.PrintStream;
+
+/**
+ * This abstract class will be used to print on the console everything needed by the client
+ * @author Riccardo
+ *
+ */
+public abstract class ClientOutputPrinter {
+	private PrintStream printer=new PrintStream(System.out,true);
+	
+	/**
+	 * @param toPrint the string to print on the console
+	 */
+	public void printLine(String toPrint) {
+		printer.println(toPrint);
+	}
+	
+	/**
+	 * 
+	 * @param num the integer number to print on the console
+	 */
+	public void printLine(int num) {
+		printer.println(num);
+	}
+	
+	/**
+	 * This method will automatically invoke the toString() method of the specified object
+	 * @param object the object on which the toString() method should be invoked
+	 */
+	public void printObject(Object object) {
+		printer.println(object);
+	}	
+}
---
+++ src/main/java/server/view/cli/ServerOutputPrinter.java
@@ -0,0 +1,35 @@
+package server.view.cli;
+
+import java.io.PrintStream;
+
+/**
+ * This abstract class will be used to print on the console all the needed and regular informations
+ * @author Riccardo
+ *
+ */
+public abstract class ServerOutputPrinter {
+	private PrintStream printer=new PrintStream(System.out,true);
+	
+	/**
+	 * @param toPrint the string to print on the console
+	 */
+	public void printLine(String toPrint) {
+		printer.println(toPrint);
+	}
+	
+	/**
+	 * 
+	 * @param num the integer number to print on the console
+	 */
+	public void printLine(int num) {
+		printer.println(num);
+	}
+	
+	/**
+	 * This method will automatically invoke the toString() method of the specified object
+	 * @param object the object on which the toString() method should be invoked
+	 */
+	public void printObject(Object object) {
+		printer.println(object);
+	}	
+}

6a4954f5bfbcb14c1069e31227314c20da9300e0
pietro di marco <pietro@MacBook-Pro-di-pietro.local>
2016-06-01 11:46:29 +0200

    Fixed the Market

--- src/test/java/board/TestMarket.java
+++ src/test/java/board/TestMarket.java
@@ -1,8 +1,8 @@
 package board;
 
-import controller.ClientSideRMIInt;
+import controller.ClientSideRMIConnectorInt;
 import controller.Player;
-import controller.SocketClientSideRMI;
+import controller.SocketConnector;
 import exceptions.UnsufficientCoinsException;
 import model.*;
 import org.junit.Test;
@@ -30,11 +30,11 @@ public class TestMarket {
 		String string="";
 		assertEquals(items.getClass(),Market.getItemsOnSale().getClass());
 		
-		ClientSideRMIInt clientSideRMIInt1 = new SocketClientSideRMI(new Socket());
+		ClientSideRMIConnectorInt clientSideRMIInt1 = new SocketConnector(new Socket());
 		Player player1 = new Player(clientSideRMIInt1,1);
-		ClientSideRMIInt clientSideRMIInt2 = new SocketClientSideRMI(new Socket());
+		ClientSideRMIConnectorInt clientSideRMIInt2 = new SocketConnector(new Socket());
 		Player player2 = new Player(clientSideRMIInt2,2);
-		ClientSideRMIInt clientSideRMIInt3 = new SocketClientSideRMI(new Socket());
+		ClientSideRMIConnectorInt clientSideRMIInt3 = new SocketConnector(new Socket());
 		Player player3 = new Player(clientSideRMIInt3,3);
 		
 		ItemFactory itemFactory = new ConcreteItemFactory();
@@ -47,8 +47,8 @@ public class TestMarket {
 		Market.putItemOnSale(item2);
 		Market.putItemOnSale(item3);
 		assertEquals(3,Market.getItemsOnSale().size());
-		
-		ClientSideRMIInt clientSideRMIInt4 = new SocketClientSideRMI(new Socket());
+
+		ClientSideRMIConnectorInt clientSideRMIInt4 = new SocketConnector(new Socket());
 		Player player4 = new Player(clientSideRMIInt1,4);
 		player4.addCoins(3);
 		try {

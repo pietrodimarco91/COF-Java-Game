fd774556fb87272f047d0e0dd6ff2c924885a099
pietro di marco <pietro@MacBook-Pro-di-pietro.local>
2016-06-01 11:39:36 +0200

    Non possiamo fare a meno della ServerSide in quanto se si usasse soltanto la ClientSide interface il Server non riuscirebbe ad ascoltare!!

--- src/main/java/controller/ServerSideRMIInt.java
+++
@@ -1,9 +0,0 @@
-package controller;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ServerSideRMIInt extends Remote {
-
-    void connect(ConnectorInt a)throws RemoteException;
-}

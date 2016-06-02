50bc54073b69e0337afd8ce8cb602b4d8a7af941
Riccardo Cannistrà <rickystream94@gmail.com>
2016-06-02 15:34:18 +0200

    Included an additional constructor in Player for test purposes.

--- src/test/java/board/TestMarket.java
+++ src/test/java/board/TestMarket.java
@@ -7,6 +7,8 @@ import controller.SocketConnector;
 import controller.Client.ClientSideRMIConnector;
 import exceptions.UnsufficientCoinsException;
 import model.*;
+import server.view.cli.ServerOutputPrinter;
+
 import org.junit.Test;
 
 import java.net.Socket;
@@ -33,39 +35,37 @@ public class TestMarket {
 		String string="";
 		assertEquals(items.getClass(),Market.getItemsOnSale().getClass());
 
-		ServerSideRMIConnector clientSideRMIInt1 = new ServerSideRMIConnector(new ClientSideRMIConnector());
-		Player player1 = new Player(clientSideRMIInt1,1);
-		ServerSideRMIConnector clientSideRMIInt2 = new ServerSideRMIConnector(new ClientSideRMIConnector());
-		Player player2 = new Player(clientSideRMIInt2,2);
-		ServerSideRMIConnector clientSideRMIInt3 = new ServerSideRMIConnector(new ClientSideRMIConnector());
-		Player player3 = new Player(clientSideRMIInt3,3);
+		Player player1 = new Player(1); //player1 has 11 coins
+		
+		Player player2 = new Player(2); //player2 has 12 coins
+	
+		Player player3 = new Player(3); //player3 has 13 coins
 		
 		ItemFactory itemFactory = new ConcreteItemFactory();
-		ItemOnSale item1 = itemFactory.createAssistantOnSale(player1, 5);
+		ItemOnSale item1 = itemFactory.createAssistantOnSale(player1, 6); //player1 has 6 coins
 		PoliticCard pc = new PoliticCard();
-		ItemOnSale item2 = itemFactory.createPoliticCardOnSale(pc, player2, 10);
+		ItemOnSale item2 = itemFactory.createPoliticCardOnSale(pc, player2, 7); //player2 has 5 coins
 		Tile pt = new PermitTile(board.getMap(),2);
-		ItemOnSale item3 = itemFactory.createPermitTileOnSale(pt, player3, 15);
+		ItemOnSale item3 = itemFactory.createPermitTileOnSale(pt, player3, 10); //player3 has 3 coins
 		Market.putItemOnSale(item1);
 		Market.putItemOnSale(item2);
 		Market.putItemOnSale(item3);
 		assertEquals(3,Market.getItemsOnSale().size());
-
-		ServerSideRMIConnector clientSideRMIInt4 = new ServerSideRMIConnector(new ClientSideRMIConnector());
-		Player player4 = new Player(clientSideRMIInt4,4);
-		player4.addCoins(3);
+		
+		Player player4 = new Player(4); //player4 has 14 coins
+		player4.removeCoins(10);; //player4 has 4 coins
 		try {
-			Market.buyItemOnSale(player4, item1);
+			Market.buyItemOnSale(player4, item1); 
 		} catch (UnsufficientCoinsException e) {
-			logger.log(Level.SEVERE, e.showError(), e);
+			ServerOutputPrinter.printLine(e.showError());
 			string=e.showError();
 		}
 		assertEquals("Unsifficient Coins! The specified coins aren't enough to perform this action",string);
-		player4.addCoins(3);
+		player4.addCoins(3); //player4 has 7 coins
 		try {
 			Market.buyItemOnSale(player4, item1);
 		} catch (UnsufficientCoinsException e) {
-			logger.log(Level.SEVERE, e.showError(), e);
+			ServerOutputPrinter.printLine(e.showError());
 		}
 		assertEquals(2,Market.getItemsOnSale().size());
 		assertEquals(1,player4.getCoins());
@@ -78,7 +78,7 @@ public class TestMarket {
 			logger.log(Level.SEVERE, e.showError(), e);
 		}
 		assertEquals(0,Market.getItemsOnSale().size());
-		assertEquals(15,player4.getCoins());
+		assertEquals(23,player4.getCoins());
 	}
 
 }

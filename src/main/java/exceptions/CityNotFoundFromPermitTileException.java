3e0f8dd53771dca3f490ea0a96b0ae3ab0ca9000
Riccardo Cannistrà <rickystream94@gmail.com>
2016-06-20 12:36:21 +0200

    Correct implementation of actions with Exceptions handling, now there's only the rollback to handle

--- src/main/java/exceptions/CityNotFoundFromPermitTileException.java
+++ src/main/java/exceptions/CityNotFoundFromPermitTileException.java
@@ -1,6 +1,6 @@
 package exceptions;
 
-public class NotFindCityFromPermitTileException extends Exception{
+public class CityNotFoundFromPermitTileException extends Exception{
 	public String showError() {
 		return "Invalid City Name! Please choose correct ID Permit Tile with correct City Name!";
 	}

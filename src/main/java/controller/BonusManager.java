12d6798bbe2c7baee5be9c9eeae808b007acb170
Gabriele Bressan <gabri.bressan@gmail.com>
2016-07-04 12:09:13 +0200

    simplify some methods and fixed other bugs

--- src/main/java/controller/BonusManager.java
+++ src/main/java/controller/BonusManager.java
@@ -4,13 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import exceptions.InvalidSlotException;
 import exceptions.TileNotFoundException;
 import model.Board;
 import model.City;
 import model.NobilityCell;
 import model.NobilityTrack;
+import model.PermitTile;
 import model.PoliticCard;
+import model.Region;
 import model.Tile;
+import server.view.cli.ServerOutputPrinter;
 
 /**
  * Created by Gabriele Bressan on 31/05/16.
@@ -87,20 +91,12 @@ public class BonusManager {
 				nobilityTrackBonus(bonus, player);
 				break;
 			case "DRAWPERMITTILE": // WRONG IMPLEMENTATION!!!
-				/*
-				 * player.addCardOnHand(new PoliticCard());
-				 * PubSub.notifyAllClients(this.players,
-				 * "Player with nickname '" + player.getNickName() +
-				 * "' won a bonus and draw a Permit tile!");
-				 */
+				drawPermitTile(player);
 				break;
 			case "BONUSPERMITTILE":
 				bonusPermitTile(bonus, player);
 				break;
-			case "TWOEMPORIUMCITY": // Must be checked: Riccardo added the first
-									// two lines of code to avoid errors if the
-									// number of controlled cities is == 0
-									// I don't like the else branch
+			case "TWOEMPORIUMCITY": 
 				twoEmporiumCityBonus(bonus, player);
 				break;
 			case "NEWMAINACTION":
@@ -112,22 +108,31 @@ public class BonusManager {
 	}
 
 	private void assistantBonus(ArrayList<String> bonus, Player player) {
-		Random randomBonus = new Random();
-		int supLimit, infLimit, numberOfBonus;
-		infLimit = 1;
-		supLimit = 5 - infLimit;
-		numberOfBonus = randomBonus.nextInt(supLimit) + infLimit;
+		int numberOfBonus;
+		numberOfBonus = randomNumber(1, 5);
 		player.addMoreAssistant(numberOfBonus);
 		PubSub.notifyAllClients(this.players,
 				"Player with nickname '" + player.getNickName() + "' won " + numberOfBonus + " Assistants!");
 	}
+	
+	private void drawPermitTile(Player player){
+		Region region[]=this.board.getRegions();
+		Tile bonusTile;
+		int slotChoice,regionChoice;
+		regionChoice = randomNumber(1, 3);
+		slotChoice = randomNumber(1, 2);
+		try {
+			bonusTile=region[regionChoice-1].getDeck().drawPermitTile(slotChoice);
+			player.addUnusedPermitTiles(bonusTile);
+			PubSub.notifyAllClients(this.players,"Player with nickname '" + player.getNickName() +"' won a bonus and draw a Permit tile!");
+		} catch (InvalidSlotException e) {
+			ServerOutputPrinter.printLine(e.getMessage());;
+		}	
+	}
 
 	private void victoryTrackBonus(ArrayList<String> bonus, Player player) {
-		Random randomBonus = new Random();
-		int supLimit, infLimit, numberOfBonus;
-		infLimit = 1;
-		supLimit = 15 - infLimit;
-		numberOfBonus = randomBonus.nextInt(supLimit) + infLimit;
+		int numberOfBonus;
+		numberOfBonus = randomNumber(1, 15);
 		player.addVictoryPoints(numberOfBonus);
 		PubSub.notifyAllClients(this.players, "Player with nickname '" + player.getNickName() + "' won " + numberOfBonus
 				+ " points for the Victory Track!");
@@ -140,22 +145,16 @@ public class BonusManager {
 	}
 
 	private void coinsBonus(ArrayList<String> bonus, Player player) {
-		Random randomBonus = new Random();
-		int supLimit, infLimit, numberOfBonus;
-		infLimit = 1;
-		supLimit = 7 - infLimit;
-		numberOfBonus = randomBonus.nextInt(supLimit) + infLimit;
+		int numberOfBonus;
+		numberOfBonus = randomNumber(1,7);
 		player.addCoins(numberOfBonus);
 		PubSub.notifyAllClients(this.players,
 				"Player with nickname '" + player.getNickName() + "' won " + numberOfBonus + " Coins!");
 	}
 
 	private void nobilityTrackBonus(ArrayList<String> bonus, Player player) {
-		Random randomBonus = new Random();
-		int supLimit, infLimit, numberOfBonus;
-		infLimit = 1;
-		supLimit = 3 - infLimit;
-		numberOfBonus = randomBonus.nextInt(supLimit) + infLimit;
+		int numberOfBonus;
+		numberOfBonus = randomNumber(1, 3);
 		player.changePositionInNobilityTrack(numberOfBonus);
 		int position = player.getPositionInNobilityTrack();
 		NobilityCell cell = this.nobilityTrack.getNobilityTrackCell(position);
@@ -216,5 +215,13 @@ public class BonusManager {
 		player.mainActionDone(false);
 		PubSub.notifyAllClients(this.players,
 				"The player with nickname: " + player.getNickName() + " won the 'NEW MAIN ACTION' bonus!");
+	}
+	
+	private int randomNumber(int infLimit,int supLimit){
+		int randomNumber;
+		Random randomBonus = new Random();
+		randomNumber= randomBonus.nextInt(supLimit) + infLimit;
+		return randomNumber;
+		
 	}
 }

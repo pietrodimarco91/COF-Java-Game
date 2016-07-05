3ab302f26f07051bdf69d684e4d3f60379e39fdc
pietro di marco <pietro@MacBook-Pro-di-pietro.local>
2016-07-05 17:58:06 +0200

    WaitingRoom

---
+++ src/main/java/client/view/gui/WaitingRoomController.java
@@ -0,0 +1,17 @@
+package client.view.gui;
+
+import client.controller.ClientGUIController;
+import javafx.stage.Stage;
+
+/**
+ * Created by pietro on 05/07/16.
+ */
+public class WaitingRoomController extends ClientGUIController{
+
+    private Stage waitingRoomStage;
+
+
+    public void setStage(Stage stage) {
+        waitingRoomStage = stage;
+    }
+}

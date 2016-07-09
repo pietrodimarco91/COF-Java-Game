f3073e9da7fafc7a431b5dc0cd3179cc40ba0b6b
Gabriele Bressan <gabri.bressan@gmail.com>
2016-07-09 23:29:24 +0200

    fixed char reply scene

--- src/main/java/client/view/gui/configurator/ChatMessage.java
+++ src/main/java/client/view/gui/configurator/ChatMessage.java
@@ -8,6 +8,8 @@ import controller.ServerSideConnectorInt;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class ChatMessage extends ClientGUIController{
@@ -20,13 +22,20 @@ public class ChatMessage extends ClientGUIController{
 	
 	
 	@FXML
-	public void replyMessage(ActionEvent event){
+	public void replyMessage(){
 		try {
 			this.connector.sendToServer(new Packet(replyMessage.getText(), "***"));
 			replyMessage.setText("");
+			this.stage.close();
 		} catch (RemoteException e) {
 		}
 		
+	}
+	@FXML
+	public void handleEnterKey(KeyEvent event){
+		if(event.getCode()==KeyCode.ENTER){
+			replyMessage();
+		}
 	}
 	
 	public void setStage(Stage stage) {

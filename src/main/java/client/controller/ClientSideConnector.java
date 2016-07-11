6b6a36ada737a2ca297174302caacab2a539d0b7
Riccardo Cannistrà <rickystream94@gmail.com>
2016-07-11 11:31:11 +0200

    Javadoc for client controller

--- src/main/java/client/controller/ClientSideConnector.java
+++ src/main/java/client/controller/ClientSideConnector.java
@@ -3,22 +3,25 @@ package client.controller;
 import client.view.cli.ClientOutputPrinter;
 import controller.ClientSideConnectorInt;
 import controller.Packet;
-
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
 /**
- * Created by pietro on 29/05/16.
+ * This class represents the remote object used by the server with an RMI
+ * connection in order to send packets to the Client.
  */
 public class ClientSideConnector extends UnicastRemoteObject implements ClientSideConnectorInt {
 
 	private transient ClientGUIController guiController;
 
 	public ClientSideConnector() throws RemoteException {
-
 	}
 
 	@Override
+	/**
+	 * This method is invoked when the Server sends a packet to the client, and
+	 * this must be interpreted to perform the different actions.
+	 */
 	public void sendToClient(Packet packet) throws RemoteException {
 		switch (packet.getHeader()) {
 		case "MESSAGESTRING":
@@ -41,6 +44,10 @@ public class ClientSideConnector extends UnicastRemoteObject implements ClientSi
 		}
 	}
 
+	/**
+	 * This method is invoked by the client gui controllers to dynamically let
+	 * the connectors know which is the current GUI controller.
+	 */
 	public void setGUIController(ClientGUIController controller) {
 		this.guiController = controller;
 	}
